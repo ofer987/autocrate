b6e92103806ae7debe8b34dc8d351df0a3d54c6a
Dan Jakob Ofer <dan@ofer.to>
2021-02-05 16:08:13 -0500

    Rename Open to Launch

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ class Keyboard {
   addEventListeners(menu) {
     this.addMoveUp(menu);
     this.addMoveDown(menu);
-    this.addOpen(menu);
+    this.addLaunch(menu);
   }
 
   addMoveUp(menu) {
@@ -91,10 +91,10 @@ class Keyboard {
     });
   }
 
-  addOpen(menu) {
+  addLaunch(menu) {
     document.addEventListener('keydown', function(event) {
       if (event.keyCode === 13 || event.keyCode == 79) {
-        menu.open();
+        menu.launch();
         menu.close();
       }
     })
